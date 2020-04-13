f5ab53f05c20e91418e86781bb7872f54cea2b2b
Masato Ohta <ohta.masato89@gmail.com>
2020-04-13 23:15:45 +0900

    Added more page

--- src/js/components/App.tsx
+++ src/js/components/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Index from "components/Index"
 import Forecast from "components/Forecast"
+import Dependencies from "components/Dependencies"
 
 export default () => {
     return (
@@ -14,6 +15,11 @@ export default () => {
             <Route
                 path="/weather/forecast/:day"
                 component={Forecast}
+            />
+            <Route
+                exact={true}
+                path="/weather/dependencies"
+                component={Dependencies}
             />
         </Router>
     )
---
+++ src/js/components/Dependencies.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export default () => {
+    return <h1>Here is the list of dependencies</h1>
+}
